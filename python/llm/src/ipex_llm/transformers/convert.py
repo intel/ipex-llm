a5072f349d5db6182ecead9964bb6f71c09085e6
Yishuo Wang <yishuo.wang@intel.com>
2025-04-09 13:53:23 +0800

    only use woq_int4 in torch 2.6

--- python/llm/src/ipex_llm/transformers/convert.py
+++ python/llm/src/ipex_llm/transformers/convert.py
@@ -1089,7 +1089,7 @@ def ggml_convert_low_bit(model, qtype, optimize_model=True,
                          embedding_qtype=None,
                          mixed_precision=False,
                          disable_optimize_pre=False):
-    if qtype == ggml_tensor_qtype["sym_int4"]:
+    if qtype == ggml_tensor_qtype["sym_int4"] and torch.__version__ >= "2.6":
         logger.info(f"sym_int4 is deprecated, use woq_int4 instead")
         logger.info(f"if you are loading saved sym_int4 low bit model, "
                     "please resaved it with woq_int4")
